618b16fcfb1e2f929be7086e4dc29d4c4794fcf7
Bliffbot <149871105+Bliffbot@users.noreply.github.com>
2024-08-15 21:08:14 +0200

    added 4.0.0 code

--- simple-password-checker.py
+++ simple-password-checker.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
+import datetime
 import math
 import sys
 import os
@@ -11,10 +12,39 @@ intents = discord.Intents.none()
 client = commands.Bot(command_prefix = "/", intents = intents)
 client.remove_command("help")
 
+formats = {
+	"reset": '\033[0m',
+	"bold": '\033[1m',
+	"italic": '\033[3m',
+	"underlined": '\033[4m',
+	"black": '\033[90m',
+	"red": '\033[91m',
+	"green": '\033[92m',
+	"yellow": '\033[93m',
+	"blue": '\033[94m',
+	"magenta": '\033[95m',
+	"cyan": '\033[96m',
+	"white": '\033[97m'
+}
+
+def logger(category_format: list, category: str, text_format: list, text: str):
+	part1 = f"{formats["reset"]}{formats["white"]}[{datetime.datetime.now().strftime('%Y.%m.%d - %H:%M:%S')}]"
+	part2 = f"{formats["reset"]}"
+	for format_part in category_format:
+		part2 = part2 + f"{formats[format_part]}"
+	part3 = f" [{category}] "
+	part4 = f"{formats[format_part]}"
+	for format_part in text_format:
+		part4 = part4 + f"{formats[format_part]}"
+	part5 = f"{text}{formats["reset"]}"
+	print(part1 + part2 + part3 + part4 + part5)
+
 
 @client.event
 async def on_ready():	
 	change_status.start()
+	logger(["yellow"], "STATUS", ["white"], f"Watching in {len(client.guilds)} Servers")
+	logger(["yellow"], "STATUS", ["white"], f"{client.user} has started")
 
 
 @tasks.loop(minutes = 5)
@@ -32,6 +62,7 @@ async def ssc(interaction: discord.Interaction, ephemeral: bool=True):
 	if interaction.user.id in [1038916475525804123]:
 		sync = await client.tree.sync()
 		await interaction.followup.send(content = f"Synced {len(sync)} commands!")
+		logger(["yellow"], "STATUS", ["white"], f"Synced {len(sync)} commands")
 	else:
 		await interaction.followup.send(f"You do not have the permission to use this command", ephemeral = ephemeral)
 
@@ -46,56 +77,45 @@ async def check(interaction: discord.Interaction,
 				digits: int,
 				characters: int,
 				guessespers: float,
-				exactly: bool=True,
-				ephemeral: bool=True):
+				exactly: bool = False,
+				ephemeral: bool = True):
 	await interaction.response.defer(ephemeral = ephemeral, thinking = True)
 
-	digits = int(digits)
-	possibilities_1 = math.pow(characters, digits)
-	possibilities_2 = 0
-	if exactly == True:
+	try:
+		digits = int(digits)
+		possibilities_1 = math.pow(characters, digits)
+		possibilities_2 = 0
+		if exactly:
 			for power in range(1, digits):
 				a = math.pow(10, power)
 				possibilities_2 = possibilities_2 + a
-	possibilities_3 = possibilities_1 + possibilities_2
+		possibilities_3 = possibilities_1 + possibilities_2
 
-	seconds = possibilities_3 / guessespers
-	minutes = seconds / 60
-	hours = minutes / 60
-	days = hours / 24
-	years = days / 365
-	kiloyears = years / 1000
-	megayears = kiloyears / 1000
-	gigayears = megayears / 1000
-	terayears = gigayears / 1000
-	petayears = terayears / 1000
+		seconds = possibilities_3 / guessespers
+		minutes = seconds / 60
+		hours = minutes / 60
+		days = hours / 24
+		years = days / 365
 
-	check_embed_1 = discord.Embed(title = "__PASSWORD CHECK__", color = 0xfba422, description = "--------- INFO ---------")
-	check_embed_2 = discord.Embed(color = 0xfba422, description = "--------- TIME ---------")
-	check_embed_1.add_field(name = "Digits", value = f"{digits:,.0f}", inline = False)
-	check_embed_1.add_field(name = "Guesses/s", value = f"{guessespers:,}", inline = False)
-	check_embed_1.add_field(name = "Possible Characters", value = f"{characters:,.0f}", inline = False)
-	check_embed_1.add_field(name = "Possibilities", value = f"{possibilities_3:,.0f}", inline = False)
-	check_embed_2.add_field(name = "Seconds", value = f"{seconds:,.2f}", inline = False)
-	if minutes >= 0.01:
+		check_embed_1 = discord.Embed(title = "__PASSWORD CHECK__", color = 0xfba422, description = "--------- INFO ---------")
+		check_embed_2 = discord.Embed(color = 0xfba422, description = "--------- TIME ---------")
+		check_embed_1.add_field(name = "Digits", value = f"{digits:,.0f}", inline = False)
+		check_embed_1.add_field(name = "Guesses/s", value = f"{guessespers:,}", inline = False)
+		check_embed_1.add_field(name = "Possible Characters", value = f"{characters:,.0f}", inline = False)
+		check_embed_1.add_field(name = "Possibilities", value = f"{possibilities_3:,.0f}", inline = False)
+
+		check_embed_2.add_field(name = "Seconds", value = f"{seconds:,.2f}", inline = False)
 		check_embed_2.add_field(name = "Minutes", value = f"{minutes:,.2f}", inline = False)
-	if hours >= 0.01:
 		check_embed_2.add_field(name = "Hours", value = f"{hours:,.2f}", inline = False)
-	if days >= 0.01:
 		check_embed_2.add_field(name = "Days", value = f"{days:,.2f}", inline = False)
-	if years >= 0.01:
 		check_embed_2.add_field(name = "Years", value = f"{years:,.2f}", inline = False)
-	if kiloyears >= 0.01:
-		check_embed_2.add_field(name = "Kiloyears", value = f"{kiloyears:,.2f}", inline = False)
-	if megayears >= 0.01:
-		check_embed_2.add_field(name = "Megayears", value = f"{megayears:,.2f}", inline = False)
-	if gigayears >= 0.01:
-		check_embed_2.add_field(name = "Gigayears", value = f"{gigayears:,.2f}", inline = False)
-	if terayears >= 0.01:
-		check_embed_2.add_field(name = "Terayears", value = f"{terayears:,.2f}", inline = False)
-	if petayears >= 0.01:
-		check_embed_2.add_field(name = "Petayears", value = f"{petayears:,.2f}", inline = False)
-	check_embed_2.set_footer(text = "by Bliffbot#7080")
-	await interaction.followup.send(embeds = [check_embed_1, check_embed_2], ephemeral = ephemeral)
+		check_embed_2.set_footer(text = "by Bliffbot | Version 4.0.0")
+		await interaction.followup.send(embeds = [check_embed_1, check_embed_2], ephemeral = ephemeral)
+		logger(["green"], "COMMAND", ["white"], f"[USER] {interaction.user} {formats["red"]}[DIGITS] {digits} {formats["yellow"]}[CHARACTERS] {characters} {formats["cyan"]}[GUESSES PER S] {guessespers} {formats["magenta"]}[EXACTLY] {exactly} {formats["green"]}[RESULT] {possibilities_3} possibilities")
+	
+	except Exception as error:
+		await interaction.followup.send(f"```[ERROR] {error}```", ephemeral = ephemeral)
+		logger(["red"], "ERROR", ["white"], str(error))
+
 
 client.run(os.environ["DISCORD_BOT_TOKEN"])
