160d95dbf674bc1bcda3fddc2b9f4fc21baaa700
Bliffbot <149871105+Bliffbot@users.noreply.github.com>
2024-08-15 19:16:17 +0200

    added 3.0.0 code
    this code is broken because i made my own dependency which i no longer have - use the latest release instead

--- simple-password-checker.py
+++ simple-password-checker.py
@@ -1,36 +1,21 @@
-import math
 import discord
-import os
-import datetime
 from discord import app_commands
 from discord.ext import commands, tasks
+import os
+import math
 
 
 intents = discord.Intents.none()
 client = commands.Bot(command_prefix = "/", intents = intents)
 client.remove_command("help")
 
-class utils():
-	red='\033[31m'
-	green='\033[32m'
-	yellow='\033[33m'
-	blue='\033[34m'
-	bold='\033[1m'
-	reset='\033[0m'
-	status = f"{reset}{yellow}[{datetime.datetime.now().strftime('%Y.%m.%d - %H:%M:%S')}]{reset}"
-
 
 @client.event
-async def on_ready():
-	await client.change_presence(status = discord.Status.online,
-								 activity=discord.Activity(
-									type=discord.ActivityType.watching,
-									name=f"in {len(client.guilds)} Servers"))
-	print(f"{utils.status} Watching in {len(client.guilds)} Servers")
-	print(f"{utils.status} {client.user} has started :)")
+async def on_ready():	
+	change_status.start()
 
 
-@tasks.loop(minutes=10)
+@tasks.loop(minutes = 5)
 async def change_status():
 	await client.change_presence(status = discord.Status.online,
 								 activity=discord.Activity(
@@ -41,14 +26,17 @@ async def change_status():
 @client.tree.command(name = "ssc", description = "Syncronises the slash commands")
 @app_commands.describe(ephemeral = "Should the use of this command be invisible to others?")
 async def ssc(interaction: discord.Interaction, ephemeral: bool=True):
-	sync = await client.tree.sync()
-	await interaction.response.send_message(f"Synced {len(sync)} commands!", ephemeral = ephemeral)
-	print(f"{utils.status} Synced {len(sync)} commands{utils.reset}")
+	await interaction.response.defer(thinking = True, ephemeral = ephemeral)
+	if interaction.user.id in [1038916475525804123]:
+		sync = await client.tree.sync()
+		await interaction.followup.send(content = f"Synced {len(sync)} commands!")
+	else:
+		await interaction.followup.send(f"You do not have the permission to use this command", ephemeral = ephemeral)
 
 
 @client.tree.command(name = "check", description = "Check your Password-Strength")
 @app_commands.describe(digits = "How long is your password",
-					   characters = "How many characters are checked - e.g. 26 lowercase + 26 uppercase letters + 10 numbers = 62",
+					   characters = "How many characters are checked - eg. 26 lowercase + 26 uppercase letters + 10 numbers = 62",
 					   guessespers = "The amount of times the attacker tries to guess your password every second",
 					   exactly = "Is it possible to enter less digits than the password is long?",
 					   ephemeral = "Should the use of this command be invisible to others?")
@@ -58,6 +46,7 @@ async def check(interaction: discord.Interaction,
 				guessespers: float,
 				exactly: bool=True,
 				ephemeral: bool=True):
+	await interaction.response.defer(ephemeral = ephemeral, thinking = True)
 
 	digits = int(digits)
 	possibilities_1 = math.pow(characters, digits)
@@ -105,8 +94,7 @@ async def check(interaction: discord.Interaction,
 	if petayears >= 0.01:
 		check_embed_2.add_field(name = "Petayears", value = f"{petayears:,.2f}", inline = False)
 	check_embed_2.set_footer(text = "by Bliffbot#7080")
-	await interaction.response.send_message(embeds = [check_embed_1, check_embed_2], ephemeral = ephemeral)
-	print(f"{utils.status} Check Command used by {interaction.user}{utils.reset}")
+	await interaction.followup.send(embeds = [check_embed_1, check_embed_2], ephemeral = ephemeral)
 
 
 client.run(os.environ["DISCORD_BOT_TOKEN"])
